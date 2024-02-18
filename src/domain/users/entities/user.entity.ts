38b95861b3be6883d7c2852c7b3451910c6ea388
Matt Spilman <matthew.spilman@umbrage.com>
2024-02-18 13:43:47 -0600

    docker compose, typeORM, user entity mapping

--- src/domain/users/entities/user.entity.ts
+++ src/domain/users/entities/user.entity.ts
@@ -1,7 +1,22 @@
+import { RegistryDates } from 'common/embeded/registry.dates.embedded';
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+@Entity()
 export class User {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   name: string;
+
+  @Column({ unique: true })
   email: string;
+
+  @Column({ unique: true })
   phone: string;
+
+  @Column()
   password: string;
+
+  @Column(() => RegistryDates, { prefix: false })
+  registryDates: RegistryDates;
 }
